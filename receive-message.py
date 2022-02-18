5df84c740f0e1b1059d6ba7d084e736c2aa63f49
ngarg-vail <ngarg@vailsys.com>
2022-02-18 09:19:03 -0600

    Update snippets based off of latest SDK changes

--- receive-message.py
+++ receive-message.py
@@ -1,19 +1,21 @@
 from __future__ import print_function
 import time
 import freeclimb
+from freeclimb.api import default_api
 import os
 import json
 from flask import Flask, request
 
-configuration = freeclimb.Configuration()
-# Configure HTTP basic authorization: fc
-configuration.username = os.environ['ACCOUNT_ID']
-configuration.password = os.environ['API_KEY']
+configuration = freeclimb.Configuration(
+    # Defining host is optional and default to https://www.freeclimb.com/apiserver
+    host     = "https://www.freeclimb.com/apiserver",
+    # Configure HTTP basic authorization: fc
+    username = os.environ['ACCOUNT_ID'],
+    password = os.environ['API_KEY']
+)
 
-# Defining host is optional and default to https://www.freeclimb.com/apiserver
-configuration.host = "https://www.freeclimb.com/apiserver"
 # Create an instance of the API class
-api_instance = freeclimb.DefaultApi(freeclimb.ApiClient(configuration))
+api_instance = default_api.DefaultApi(freeclimb.ApiClient(configuration))
 
 app = Flask(__name__)
 
@@ -22,7 +24,7 @@ app = Flask(__name__)
 def incomingSms():
     if request.method == 'POST':
         message = "Hello! You texted FreeClimb's Python SDK the following: " + request.json['text']
-        message_request = freeclimb.MessageRequest(to=request.json['from'], _from=request.json['to'], text=message)
+        message_request = freeclimb.MessageRequest(_from=request.json['from'], to=request.json['to'], text=message)
         api_instance.send_an_sms_message(message_request=message_request)
 
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
@@ -31,4 +33,4 @@ def incomingSms():
 # Specify this route with 'STATUS CALLBACK URL' in App Config
 @app.route('/status', methods=['POST'])
 def status():
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
